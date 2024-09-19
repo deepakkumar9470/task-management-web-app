27ee9020c7f390567f335af4f40e3d34ef69633a
deepakkumar9470 <dk397787@gmail.com>
2024-09-19 11:48:09 +0530

    cors issue fix

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended : true}))
 app.use(cors({
-    origin: '*', 
+    origin: ['https://task-management-web-app-1-5em0.onrender.com'], 
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
